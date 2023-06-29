ae6098ccb9b3c64dc34a04c1520799a6a64dda56
priyanshu Yadav <priyanshuyadav28027@gmail.com>
2023-06-29 17:15:07 +0530

    insertion sort code added

--- 10_searchingAndSortingWeek4/04_sorting.cpp
+++ 10_searchingAndSortingWeek4/04_sorting.cpp
@@ -72,15 +72,55 @@ void bubbleSort(vector<int> arr) {
   
 }
 
+// insertion sort 
+void insertionSort(vector<int> arr) {
+    // outer loop for rounds 
+    for (int rounds = 1; rounds < arr.size(); rounds++)
+    {
+        // step 1. fetch the element
+        int val = arr[rounds];
+
+        // step 2. compare
+        int j = rounds - 1;
+        for (; j >= 0; j--)
+        {
+            // condition and shift the element
+            if (arr[j] > val) 
+            {
+                // step 3. shift
+                arr[j + 1] = arr[j];
+            }
+            else {
+                // stop  
+                break;
+            }
+            
+        }
+
+        // step 4 copy 
+        arr[j + 1] = val;
+        
+    }
+
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cout << arr[i] << " ";
+    }
+    
+    
+}
+
 int main() {
     // there are many algorithms to sort the elements in an array 
     // 1. Selection Sort -> max element swap
     // vector<int> arr = {5, 10, 3, 2, 1, 8};
     // selectionSort(arr);
 
-    vector<int> arr = {4, 2, 1,99, 8, 9, 7, 10};
-    bubbleSort(arr);
+    // vector<int> arr = {4, 2, 1,99, 8, 9, 7, 10};
+    // bubbleSort(arr);
 
+    vector<int> arr = {10, 1, 7, 6, 14, 9};
+    insertionSort(arr);
 
     
 
