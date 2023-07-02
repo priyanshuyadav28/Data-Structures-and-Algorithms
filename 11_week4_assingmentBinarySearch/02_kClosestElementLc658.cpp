99f5039c0a48a4c3a66151cc45cf31fd5c0540a3
priyanshu Yadav <priyanshuyadav28027@gmail.com>
2023-07-02 16:56:42 +0530

    binarySearch method to find k closet elements

--- 11_week4_assingmentBinarySearch/02_kClosestElementLc658.cpp
+++ 11_week4_assingmentBinarySearch/02_kClosestElementLc658.cpp
@@ -4,6 +4,7 @@
 
 using namespace std;
 
+// two pointor method o(N)
 vector<int> kClosetElement(vector<int> arr, int k , int x) {
 
     sort(arr.begin(), arr.end());
@@ -33,6 +34,64 @@ vector<int> kClosetElement(vector<int> arr, int k , int x) {
     return ans;
 }
 
+
+// binary search method 
+int lowerBound(vector<int> arr, int x) {
+    int start = 0;
+    int end = arr.size() - 1;
+    int ans = end;
+
+    while (start <= end)
+    {
+        int mid = start + (end - start) / 2;
+
+        if (arr[mid] >= x)
+        {
+            ans = mid;
+            end = mid - 1;
+        }
+        else if (x > arr[mid])
+        {
+            start = mid + 1;
+        }
+        else {
+            end = mid - 1;
+        }
+
+    }
+    return ans;
+    
+}
+
+vector<int> bs_method(vector<int> arr, int k, int x) {
+
+    int h = lowerBound(arr, x);
+    int l = h - 1;
+    
+
+    while (k--)
+    {
+        if (l < 0)
+        {
+            h++;
+        }
+
+        else if (h >= arr.size())
+        {
+            l--;
+        }
+        
+        
+        else if (x - arr[l] > arr[h] - x)
+        {
+            h++;
+        } else {
+            l--;
+        }
+        
+    }
+    return vector<int>(arr.begin() + l + 1, arr.begin() + h);
+}
 
 int main() {
 
