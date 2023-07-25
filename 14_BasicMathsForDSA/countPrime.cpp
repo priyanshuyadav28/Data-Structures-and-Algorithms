d858765434953627cd8f1a20696f54fc36b71907
priyanshu Yadav <priyanshuyadav28027@gmail.com>
2023-07-25 16:42:29 +0530

    algorithm to find the number of primes

--- 14_BasicMathsForDSA/countPrime.cpp
+++ 14_BasicMathsForDSA/countPrime.cpp
@@ -54,22 +54,23 @@ int primeCount(int n) {
 
     vector<bool> prime(n, true); // all are marked prime alredy
 
-    prime[0] = prime[1] = false;
+    prime[0] = prime[1] = false; // since 0 and 1 are not prime 
+    // the smallest prime number is 2 
 
     int ans = 0;
 
     for (int i = 2; i < n; i++)
     {
-        if (prime[i])
+        if (prime[i])  // if true in prime array 
         {
             ans++;
 
-            int j = 2 * i;
+            int j = 2 * i; // since table starts from 2 and we have to mark all the multiple of prime false 
 
             while (j < n)
             {
                 prime[j] = false;
-                j += i;
+                j += i; // since 2 * i + i will be eqaual to the next multiple
             }
             
             
