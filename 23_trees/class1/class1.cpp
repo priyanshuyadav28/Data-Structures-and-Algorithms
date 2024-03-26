57a603b59b9167e4d2f7f7ca6df92e6a746066f3
priyanshu Yadav <priyanshuyadav28027@gmail.com>
2024-03-26 16:31:19 +0530

    Add maxDepth function to calculate the maximum depth of a binary tree

--- 23_trees/class1/class1.cpp
+++ 23_trees/class1/class1.cpp
@@ -122,6 +122,20 @@ void postOrderTraversal(Node *root)
     cout << root->data << " ";
 }
 
+int maxDepth(Node *root)
+{
+    if (root == NULL)
+    {
+        return 0;
+    }
+
+    int left = maxDepth(root->left);
+    int right = maxDepth(root->right);
+
+    int ans = max(left, right) + 1;
+    return ans;
+}
+
 int main() {
 
     Node *root = NULL; 
