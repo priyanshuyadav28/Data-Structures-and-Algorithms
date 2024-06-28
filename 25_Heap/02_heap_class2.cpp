ed0783251734ebd4feee451d6c56befd5e7d61e7
priyanshu Yadav <priyanshuyadav28027@gmail.com>
2024-06-28 18:04:02 +0530

    operations on heap using stl

---
+++ 25_Heap/02_heap_class2.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+
+
+int main() {
+
+    // creates a Max Heap
+    // priority_queue<int> pq;
+
+    // pq.push(4);
+    // pq.push(3);
+    // pq.push(8);
+    // pq.push(6);
+    // pq.push(9);
+
+    // cout << "Top Element: " << pq.top() << endl;
+    // pq.pop();
+    // cout << "Top Element: " << pq.top() << endl;
+    // pq.pop();
+    // cout << "Top Element: " << pq.top() << endl;
+    // pq.pop();
+    // cout << "Top Element: " << pq.top() << endl;
+    // pq.pop();
+    // cout << "Top Element: " << pq.top() << endl;
+    // pq.pop();
+
+    // cout << "Size is " << pq.size() << endl;
+
+    // if (pq.empty()) {
+    //     cout << "Max Heap is Empty " << endl;
+    // } else {
+    //     cout << "Max Heap is Not Empty " << endl;
+    // }
+
+    // creating a MIN  Heap 
+    // priority_queue<int, vector<int>, greater<int> > mh;
+
+    // mh.push(4);
+    // mh.push(3);
+    // mh.push(8);
+    // mh.push(6);
+    // mh.push(9);
+
+    // cout << "Top Element: " << mh.top() << endl;
+    // mh.pop();
+    // cout << "Top Element: " << mh.top() << endl;
+    // mh.pop();
+    // cout << "Top Element: " << mh.top() << endl;
+    // mh.pop();
+    // cout << "Top Element: " << mh.top() << endl;
+    // mh.pop();
+    // cout << "Top Element: " << mh.top() << endl;
+    // mh.pop();
+
+    // cout << "Size is " << mh.size() << endl;
+
+    // if (mh.empty())
+    // {
+    //     cout << "Min Heap is Empty " << endl;
+    // }
+    // else
+    // {
+    //     cout << "Min Heap is Not Empty " << endl;
+    // }
+
+    
+
+
+    return 0; 
+}
